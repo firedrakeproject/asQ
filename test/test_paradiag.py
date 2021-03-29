e7a2fcc61701a129b3190483b93bec3d8ba33031
Werner Bauer <werner.bauer.email@gmail.com>
2021-03-29 16:52:41 +0100

    add new test to check that nl heat with either fft or direct PC gives same results

--- test/test_paradiag.py
+++ test/test_paradiag.py
@@ -530,3 +530,78 @@ def test_diag_precon_mixed():
         err.assign(un-pun)
         assert(fd.norm(err) < 1.0e-15)
 
+def test_diag_precon_nl():
+    #Test PCDIAGFFT by using it within the relaxation method
+    #using the NONLINEAR heat equation as an example
+    #we compare one iteration using just the diag PC
+    #with the direct solver
+
+    mesh = fd.UnitSquareMesh(20,20)
+    V = fd.FunctionSpace(mesh, "CG", 1)
+
+    x, y = fd.SpatialCoordinate(mesh)
+    u0 = fd.Function(V).interpolate(fd.exp(-((x-0.5)**2 + (y-0.5)**2)/0.5**2))
+    dt = 0.01
+    theta = 0.5
+    alpha = 0.01
+    M = 4
+    c = fd.Constant(0.1)
+
+    diagfft_options = {
+        'ksp_type':'preonly',
+        'pc_type':'lu',
+        'pc_factor_mat_solver_type':'mumps',
+        'mat_type':'aij'}
+
+    solver_parameters = {
+        'snes_monitor':None,
+        'snes_converged_reason':None,
+        'mat_type':'matfree',
+        'ksp_rtol':1.0e-10,
+        'ksp_max_it':12,
+        'ksp_converged_reason':None,
+        'pc_type':'python',
+        'pc_python_type':'asQ.DiagFFTPC',
+        'diagfft':diagfft_options}
+
+    def form_function(u, v):
+        return fd.inner((1.+c*fd.inner(u,u))*fd.grad(u), fd.grad(v))*fd.dx
+
+    def form_mass(u, v):
+        return u*v*fd.dx
+
+    PD = asQ.paradiag(form_function=form_function,
+                           form_mass=form_mass, W=V,
+                           w0=u0, dt=dt, theta=theta,
+                           alpha=alpha, M=M,
+                           solver_parameters=solver_parameters,
+                           circ="outside", tol=1.0e-12,
+                           maxits=1)
+    PD.solve(verbose=True)
+    solver_parameters = {'ksp_type':'preonly', 'pc_type':'lu',
+                         'pc_factor_mat_solver_type':'mumps',
+                         'mat_type':'aij',
+                         # 'snes_monitor':None,
+                         # 'snes_converged_reason':None,
+                         }
+    PDe = asQ.paradiag(form_function=form_function,
+                            form_mass=form_mass, W=V,
+                            w0=u0, dt=dt, theta=theta,
+                            alpha=alpha, M=M,
+                            solver_parameters=solver_parameters,
+                            circ="outside", tol=1.0e-12,
+                            maxits=1)
+    PDe.solve(verbose=True)
+    unD = fd.Function(V, name='diag')
+    un = fd.Function(V, name='full')
+    err = fd.Function(V, name='error')
+    unD.assign(u0)
+    un.assign(u0)
+    for i in range(M):
+        walls = PD.w_all.split()[i]
+        wallsE = PDe.w_all.split()[i]
+        unD.assign(walls)
+        un.assign(wallsE)
+        err.assign(un-unD)
+        # print(fd.norm(err))
+        assert(fd.norm(err) < 1.0e-12)
