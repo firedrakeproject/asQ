82a495b2151202274dc2ad76397ade31170f1c87
Colin Cotter <colin.cotter@imperial.ac.uk>
2022-02-03 18:20:19 +0000

    made a separate module for the fft pc that also contains some stuff to deal with passing in the applications context.

--- asQ/__init__.py
+++ asQ/__init__.py
@@ -1,1 +1,2 @@
-from .paradiag import paradiag, DiagFFTPC  # noqa: F401
+from .paradiag import paradiag  # noqa: F401
+from .diag_preconditioner import get_context, set_context, DiagFFTPC  # noqa: F401
