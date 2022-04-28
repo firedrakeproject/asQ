1d7506a4d5c1089b9c8cdd68c922d8d7ee026ee9
JHopeCollins <joshua.hope-collins13@imperial.ac.uk>
2022-04-28 17:19:01 +0100

    new implicit scheme from sw_implicit for diagonal block solves in step-(b)

--- examples/swim.py
+++ examples/swim.py
@@ -17,7 +17,7 @@ parser.add_argument('--coords_degree', type=int, default=1, help='Degree of poly
 parser.add_argument('--degree', type=int, default=1, help='Degree of finite element space (the DG space).')
 parser.add_argument('--kspschur', type=int, default=1, help='Number of KSP iterations on the Schur complement.')
 parser.add_argument('--kspmg', type=int, default=3, help='Number of KSP iterations in the MG levels.')
-parser.add_argument('--tlblock', type=str, default='patch', help='Solver for the velocity-velocity block. mg==Multigrid with patchPC, lu==direct solver with MUMPS, patch==just do a patch smoother. Default is patch')
+parser.add_argument('--tlblock', type=str, default='mg', help='Solver for the velocity-velocity block. mg==Multigrid with patchPC, lu==direct solver with MUMPS, patch==just do a patch smoother. Default is mg')
 parser.add_argument('--show_args', action='store_true', help='Output all the arguments.')
 
 args = parser.parse_known_args()
@@ -33,21 +33,45 @@ base_level = args.base_level
 nrefs = args.ref_level - base_level
 filename = args.filename
 deg = args.coords_degree
+#deg = max( args.coords_degree, args.degree+1 )
 distribution_parameters = {"partition": True, "overlap_type": (fd.DistributedMeshOverlapType.VERTEX, 2)}
 
 nspatial_domains = 1
 ensemble = fd.Ensemble(fd.COMM_WORLD, nspatial_domains)
 
+# mesh set up
+
+def high_order_mesh_hierarchy(mh, degree, R0):
+    meshes = []
+    for m in mh:
+        X = fd.VectorFunctionSpace(m, "Lagrange", degree)
+        new_coords = fd.interpolate(m.coordinates, X)
+        x, y, z = new_coords
+        r = (x**2 + y**2 + z**2)**0.5
+        new_coords.assign(R0*new_coords/r)
+        new_mesh = fd.Mesh(new_coords)
+        meshes.append(new_mesh)
+
+    return fd.HierarchyBase(meshes, mh.coarse_to_fine_cells,
+                            mh.fine_to_coarse_cells,
+                            mh.refinements_per_level, mh.nested)
+
+# mesh heirarchy from sw_implicit
 if args.tlblock == "mg":
-    basemesh = fd.IcosahedralSphereMesh(radius=R0,
-                                        refinement_level=base_level,
-                                        degree=deg,
-                                        distribution_parameters=
-                                        distribution_parameters,
-                                        comm=ensemble.comm)
+    basemesh = fd.IcosahedralSphereMesh(radius = R0,
+                                        refinement_level = base_level,
+                                        degree = deg,
+                                        distribution_parameters = distribution_parameters,
+                                        comm = ensemble.comm)
+    del basemesh._radius
     mh = fd.MeshHierarchy(basemesh, nrefs)
+    mh = high_order_mesh_hierarchy(mh, deg, R0)
     for mesh in mh:
+        xf = mesh.coordinates
+        mesh.transfer_coordinates = fd.Function(xf)
         x = fd.SpatialCoordinate(mesh)
+        r = (x[0]**2 + x[1]**2 + x[2]**2)**0.5
+        xf.interpolate(R0*xf/r)
         mesh.init_cell_orientations(x)
     mesh = mh[-1]
 else:
@@ -65,10 +89,6 @@ x = fd.SpatialCoordinate(mesh)
 
 outward_normals = fd.CellNormal(mesh)
 
-def perp(u):
-    return fd.cross(outward_normals, u)
-
-
 degree = args.degree
 V1 = fd.FunctionSpace(mesh, "BDM", degree+1)
 V2 = fd.FunctionSpace(mesh, "DG", degree)
@@ -81,10 +101,13 @@ g = fd.Constant(9.8)  # Gravitational constant
 b = fd.Function(V2, name="Topography")
 # D = eta + b
 
+# nonlinear swe forms
+
+def perp(u):
+    return fd.cross(outward_normals, u)
 
 def both(u):
     return 2*fd.avg(u)
-
 
 def form_function(u, h, v, q):
     K = 0.5*fd.inner(u, u)
@@ -108,28 +131,10 @@ def form_function(u, h, v, q):
 def form_mass(u, h, v, q):
     return fd.inner(u, v)*fd.dx + h*q*fd.dx
 
-# Parameters for the diag
-sparameters = {
-    "ksp_type": "preonly",
-    'pc_python_type': 'lu',
-    'pc_factor_mat_solver_type': 'mumps'}
-
-solver_parameters_diag = {
-    "snes_linesearch_type": "basic",
-    'snes_monitor': None,
-    'snes_converged_reason': None,
-    'mat_type': 'matfree',
-    'ksp_type': 'gmres',
-    'ksp_monitor': None,
-    'pc_type': 'python',
-    'pc_python_type': 'asQ.DiagFFTPC'}
-
-M = [2, 2, 2, 2]
-for i in range(sum(M)): # should this be sum(M) or max(M)?
-    solver_parameters_diag["diagfft_"+str(i)+"_"] = sparameters
-
 dt = 60*60*args.dt
 t = 0.
+
+# initial conditions
 
 u_0 = 20.0  # maximum amplitude of the zonal wind [m/s]
 u_max = fd.Constant(u_0)
@@ -152,6 +157,73 @@ minarg = fd.Min(pow(rl, 2),
 bexpr = 2000.0*(1 - fd.sqrt(minarg)/rl)
 b.interpolate(bexpr)
 
+# solver parameters
+
+# parameters for the implicit diagonal solve in step-(b)
+sparameters_orig = {
+    #"ksp_converged_reason": None,
+    "ksp_type": "preonly",
+    'pc_python_type': 'lu',
+    'pc_factor_mat_solver_type': 'mumps'}
+
+sparameters_new = {
+    #"snes_monitor": None,
+    "mat_type": "matfree",
+    "ksp_type": "preonly",
+    #"ksp_monitor_true_residual": None,
+    #"ksp_converged_reason": None,
+    "ksp_atol": 1e-8,
+    "ksp_rtol": 1e-8,
+    "ksp_max_it": 400,
+    "pc_type": "mg",
+    "pc_mg_cycle_type": "v",
+    "pc_mg_type": "multiplicative",
+    "mg_levels_ksp_type": "gmres",
+    "mg_levels_ksp_max_it": 5,
+    #"mg_levels_ksp_convergence_test": "skip",
+    "mg_levels_pc_type": "python",
+    "mg_levels_pc_python_type": "firedrake.PatchPC",
+    "mg_levels_patch_pc_patch_save_operators": True,
+    "mg_levels_patch_pc_patch_partition_of_unity": True,
+    "mg_levels_patch_pc_patch_sub_mat_type": "seqdense",
+    "mg_levels_patch_pc_patch_construct_codim": 0,
+    "mg_levels_patch_pc_patch_construct_type": "vanka",
+    "mg_levels_patch_pc_patch_local_type": "additive",
+    "mg_levels_patch_pc_patch_precompute_element_tensors": True,
+    "mg_levels_patch_pc_patch_symmetrise_sweep": False,
+    "mg_levels_patch_sub_ksp_type": "preonly",
+    "mg_levels_patch_sub_pc_type": "lu",
+    "mg_levels_patch_sub_pc_factor_shift_type": "nonzero",
+    "mg_coarse_pc_type": "python",
+    "mg_coarse_pc_python_type": "firedrake.AssembledPC",
+    "mg_coarse_assembled_pc_type": "lu",
+    "mg_coarse_assembled_pc_factor_mat_solver_type": "mumps",
+}
+
+sparameters = sparameters_new
+
+solver_parameters_diag = {
+    "snes_linesearch_type": "basic",
+    'snes_monitor': None,
+    'snes_converged_reason': None,
+    'mat_type': 'matfree',
+    'ksp_type': 'gmres',
+    #'ksp_type': 'preonly',
+    #'ksp_max_it': 5,
+    'ksp_monitor': None,
+    #"ksp_monitor_true_residual": None,
+    "ksp_converged_reason": None,
+    'pc_type': 'python',
+    'pc_python_type': 'asQ.DiagFFTPC'}
+
+M = [1, 1, 1, 1]
+#M = [2, 2, 2, 2]
+#M = [3, 3, 3, 3]
+#M = [4, 4, 4, 4]
+
+for i in range(sum(M)): # should this be sum(M) or max(M)?
+    solver_parameters_diag["diagfft_"+str(i)+"_"] = sparameters
+
 alpha = args.alpha
 theta = 0.5
 
@@ -164,6 +236,7 @@ PD = asQ.paradiag(ensemble=ensemble,
                   circ="quasi",
                   jac_average="newton", tol=1.0e-6, maxits=None,
                   ctx={}, block_mat_type="aij")
+
 PD.solve()
 
 u_out = fd.Function(V1,name='velocity')
@@ -173,7 +246,8 @@ h_out = fd.Function(V2,name='depth')
 r = PD.ensemble.ensemble_comm.rank
 if r == len(M) - 1:
     file0 = fd.File("output/"+filename+".pvd", comm=ensemble.comm)
-    u0, h0, u1, h1 = PD.w_all.split()
-    u_out.assign(u1)
-    h_out.assign(h1)
+    #u0, h0, u1, h1 = PD.w_all.split()
+    fields = PD.w_all.split()
+    u_out.assign(fields[-2])
+    h_out.assign(fields[-1])
     file0.write(u_out, h_out)
