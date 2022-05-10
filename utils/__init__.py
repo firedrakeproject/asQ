6529a466a0e7398bfc41bc48e9cb83c414b0de85
JHopeCollins <joshua.hope-collins13@imperial.ac.uk>
2022-05-10 15:01:28 +0100

    initial utils commit. SI units and scales

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+#import firedrake_utils.planets  # noqa: F401
+#import firedrake_utils.shallow_water  # noqa: F401
+import firedrake_utils.units  # noqa: F401
+#import firedrake_utils.mg  # noqa: F401
