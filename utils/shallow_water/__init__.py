d72ce632c8f7711a895bf7369d256d0809b62cd7
JHopeCollins <joshua.hope-collins13@imperial.ac.uk>
2022-05-10 15:11:09 +0100

    __init__ files for shallow water directory tree

---
+++ utils/shallow_water/__init__.py
@@ -0,0 +1,3 @@
+import firedrake_utils.shallow_water.linear  # noqa: F401
+import firedrake_utils.shallow_water.nonlinear  # noqa: F401
+import firedrake_utils.shallow_water.williamson1992  # noqa: F401
---
+++ utils/shallow_water/williamson1992/__init__.py
@@ -0,0 +1,2 @@
+import firedrake_utils.shallow_water.williamson1992.case2  # noqa: F401
+import firedrake_utils.shallow_water.williamson1992.case5  # noqa: F401
