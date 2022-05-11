7b9459e4abd642457678eaef485a8c88aef0d31c
JHopeCollins <joshua.hope-collins13@imperial.ac.uk>
2022-05-11 12:54:43 +0100

    remove icosahedral mesh function - it only wraps specifying the radius, and is reduntant to the mg mesh function

--- utils/planets/earth.py
+++ utils/planets/earth.py
@@ -20,24 +20,3 @@ Omega = fd.Constant(omega)
 # gravitational acceleration
 gravity = 9.80616*units.metre/(units.second*units.second)
 Gravity = fd.Constant(gravity)
-
-# # # === --- planetary mesh --- === # # #
-
-
-def IcosahedralMesh(refinement_level=0,
-                    degree=1,
-                    reorder=None,
-                    distribution_parameters=None,
-                    comm=fd.COMM_WORLD):
-
-    globe = \
-        fd.IcosahedralSphereMesh(radius=radius,
-                                 refinement_level=refinement_level,
-                                 degree=degree,
-                                 reorder=reorder,
-                                 distribution_parameters=distribution_parameters,
-                                 comm=comm)
-
-    globe.init_cell_orientations(fd.SpatialCoordinate(globe))
-
-    return globe
