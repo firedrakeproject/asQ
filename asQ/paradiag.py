098e1d0def5a16507fdee27c9d8ea8af229438e7
JHopeCollins <joshua.hope-collins13@imperial.ac.uk>
2022-05-17 10:35:27 +0100

    resetting for next_window from last window uses the ensemble bcast instead of a isend round

--- asQ/paradiag.py
+++ asQ/paradiag.py
@@ -266,7 +266,7 @@ class paradiag(object):
         """
         Reset paradiag ready for next time-window
         :arg w1: initial solution for next time-window.If None,
-                 will use the last timestep from previous window
+                 will use the final timestep from previous window
         """
         rank = self.rT
         ncomm = self.ensemble.ensemble_comm.size
@@ -274,28 +274,20 @@ class paradiag(object):
 
         if w1 is not None:  # use given function
             self.w0.assign(w1)
-        else:  # last rank broadcasts last timestep
-            # communication should all be done with a broadcast
-            # need to find how to bcast with the ensemble
-            mpi_requests = []
+        else:  # last rank broadcasts final timestep
             if rank == ncomm-1:
+                # index of start of final timestep
+                i0 = ncpts*(self.M[-1]-1)
                 for k in range(ncpts):
-                    # index of start of last timestep
-                    i0 = ncpts*(self.M[-1]-1)
                     wend = self.w_alls[i0+k]
                     self.w0.sub(k).assign(wend)
-                for r in range(len(self.M)-1):
-                    request_send = self.ensemble.isend(self.w0,
-                                                       dest=r,
-                                                       tag=r)
-                    mpi_requests.extend(request_send)
-            else:
-                request_recv = self.ensemble.irecv(self.w0,
-                                                   source=ncomm-1,
-                                                   tag=rank)
-                mpi_requests.extend(request_recv)
-            MPI.Request.Waitall(mpi_requests)
 
+            # there should really be a bcast method on the ensemble
+            # if this gets added in firedrake then this will need updating
+            for dat in self.w0.dat:
+                self.ensemble.ensemble_comm.Bcast(dat.data, root=ncomm-1)
+
+        # persistence forecast
         for i in range(self.M[rank]):
             for k in range(ncpts):
                 self.w_alls[ncpts*i+k].assign(self.w0.sub(k))
