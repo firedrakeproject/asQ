665bbc4080da8f4593d45de568e4b111d04a373c
JHopeCollins <joshua.hope-collins13@imperial.ac.uk>
2022-09-21 17:00:09 +0100

    initial minimal serial miniapp

---
+++ examples/serial/shallow_water/galewsky.py
@@ -0,0 +1,146 @@
+
+import firedrake as fd
+from firedrake.petsc import PETSc
+
+from utils import units
+from utils import mg
+from utils.planets import earth
+import utils.shallow_water as swe
+from utils.shallow_water import galewsky
+
+from utils.serial import SerialMiniApp
+
+from sys import exit
+
+PETSc.Sys.popErrorHandler()
+
+# get command arguments
+import argparse
+parser = argparse.ArgumentParser(
+    description='Galewsky testcase using fully implicit SWE solver.',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+
+parser.add_argument('--ref_level', type=int, default=2, help='Refinement level of icosahedral grid.')
+parser.add_argument('--nt', type=int, default=10, help='Number of time steps.')
+parser.add_argument('--dt', type=float, default=0.5, help='Timestep in hours.')
+parser.add_argument('--degree', type=float, default=swe.default_degree(), help='Degree of the depth function space.')
+parser.add_argument('--theta', type=float, default=0.5, help='Parameter for implicit theta method. 0.5 for trapezium rule, 1 for backwards Euler.')
+parser.add_argument('--filename', type=str, default='galewsky', help='Name of output vtk files')
+parser.add_argument('--show_args', action='store_true', help='Output all the arguments.')
+
+args = parser.parse_known_args()
+args = args[0]
+
+if args.show_args:
+    PETSc.Sys.Print(args)
+
+PETSc.Sys.Print('')
+PETSc.Sys.Print('### === --- Setting up --- === ###')
+PETSc.Sys.Print('')
+
+# icosahedral mg mesh
+mesh = swe.create_mg_globe_mesh(ref_level=args.ref_level)
+x = fd.SpatialCoordinate(mesh)
+
+# time step
+dt = args.dt*units.hour
+
+# shallow water equation function spaces (velocity and depth)
+W = swe.default_function_space(mesh, degree=args.degree)
+
+# parameters
+gravity = earth.Gravity
+
+topography = galewsky.topography_expression(*x)
+coriolis = swe.earth_coriolis_expression(*x)
+
+# initial conditions
+w_initial = fd.Function(W)
+u_initial, h_initial = w_initial.split()
+
+u_initial.project(galewsky.velocity_expression(*x))
+h_initial.project(galewsky.depth_expression(*x))
+
+# current and next timestep
+w0 = fd.Function(W).assign(w_initial)
+w1 = fd.Function(W).assign(w_initial)
+
+# shallow water equation forms
+def form_function(u, h, v, q):
+    return swe.nonlinear.form_function(mesh,
+                                       gravity,
+                                       topography,
+                                       coriolis,
+                                       u, h, v, q)
+
+
+def form_mass(u, h, v, q):
+    return swe.nonlinear.form_mass(mesh, u, h, v, q)
+
+
+# solver parameters for the implicit solve
+sparameters = {
+    'snes': {
+        'monitor': None,
+        'converged_reason': None,
+        'rtol': 1e-12
+    },
+    'mat_type': 'matfree',
+    'ksp_type': 'fgmres',
+    'ksp': {
+        'atol': 1e-8,
+        'rtol': 1e-8,
+        'monitor': None,
+        'converged_reason': None
+    },
+    'pc_type': 'mg',
+    'pc_mg_cycle_type': 'w',
+    'pc_mg_type': 'multiplicative',
+    'mg': {
+        'levels': {
+            'ksp_type': 'gmres',
+            'ksp_max_it': 5,
+            'pc_type': 'python',
+            'pc_python_type': 'firedrake.PatchPC',
+            'patch': {
+                'pc_patch_save_operators': True,
+                'pc_patch_partition_of_unity': True,
+                'pc_patch_sub_mat_type': 'seqdense',
+                'pc_patch_construct_codim': 0,
+                'pc_patch_construct_type': 'vanka',
+                'pc_patch_local_type': 'additive',
+                'pc_patch_precompute_element_tensors': True,
+                'pc_patch_symmetrise_sweep': False,
+                'sub_ksp_type': 'preonly',
+                'sub_pc_type': 'lu',
+                'sub_pc_factor_shift_type': 'nonzero',
+            },
+        },
+        'coarse': {
+            'pc_type': 'python',
+            'pc_python_type': 'firedrake.AssembledPC',
+            'assembled_pc_type': 'lu',
+            'assembled_pc_factor_mat_solver_type': 'mumps',
+        },
+    }
+}
+
+# set up nonlinear solver
+miniapp = SerialMiniApp(dt, args.theta,
+                        w_initial,
+                        form_mass,
+                        form_function,
+                        sparameters)
+
+miniapp.nlsolver.set_transfer_manager(
+    mg.manifold_transfer_manager(W))
+
+PETSc.Sys.Print('### === --- Timestepping loop --- === ###')
+
+def preproc(app, step, t):
+    PETSc.Sys.Print('')
+    PETSc.Sys.Print(f'=== --- Timestep {step} --- ===')
+    PETSc.Sys.Print('')
+
+miniapp.solve(args.nt, preproc=preproc)
