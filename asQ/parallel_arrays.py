442367c1709912fe78bbb14ff5762eeb106556ed
JHopeCollins <joshua.hope-collins13@imperial.ac.uk>
2022-10-12 13:00:55 +0100

    DistributedDataLayout and updated array

--- asQ/parallel_arrays.py
+++ asQ/parallel_arrays.py
@@ -1,79 +1,156 @@
 from pyop2.mpi import MPI
-from numpy import zeros as zero_array
+from numpy import zeros, cumsum
 
 
-class DistributedArray(object):
-    def __init__(self, local_size, dtype=None, comm=MPI.COMM_WORLD):
+def in_range(i, length, throws=False):
+    '''
+    Is the index i within the range of length?
+    :arg i: index to check
+    :arg length: the number of elements in the range
+    :arg throws: if True, then an IndexError is raised if the index is out of range
+    '''
+    result = (-length <= i < length)
+    if throws and result is False:
+        raise IndexError(f"Index {i} is outside the range {length}")
+    return result
+
+
+class DistributedDataLayout(object):
+    def __init__(self, partition, comm=MPI.COMM_WORLD):
         '''
-        1D array distributed over an MPI comm.
+        A representation of a 1D set of data distributed over several MPI ranks.
 
-        Each rank has a copy of the entire array of size comm.size*size_per_rank.
-        Each rank owns the array slice [comm.rank*local_size:(comm.rank+1)*local_size].
+        :arg partition: The number of data elements on each rank. Can be a list of integers, in which case len(partition) must be comm.size. Can be a single integer, in which case all ranks have the same number of elements.
+        :arg comm: MPI communicator the data is distributed over.
+        '''
+        if isinstance(partition, int):
+            partition = [partition for _ in range(comm.size)]
+        else:
+            if len(partition) != comm.size:
+                raise ValueError(f"Partition size {len(partition)} not equal to comm size {comm.size}")
+        self.partition = partition
+        self.comm = comm
+        self.rank = comm.rank
+        self.local_size = partition[self.rank]
+        self.global_size = sum(partition)
+        self.offset = sum(partition[:self.rank])
+
+    def shift_index(self, i, itype='l', rtype='l'):
+        '''
+        Shift index between local and global addressing, and account for -ve indices.
+
+        For example if there are 3 ranks each owning two elements then:
+            global indices 0,1 are local indices 0,1 on rank 0.
+            global indices 2,3 are local indices 0,1 on rank 1.
+            global indices 4,5 are local indices 0,1 on rank 2.
+        Throws IndexError if original or shifted index is out of bounds.
+
+        :arg i: index to shift.
+        :arg itype: type of index i. 'l' for local, 'g' for global.
+        :arg rtype: type of returned shifted index. 'l' for local, 'g' for global.
+        '''
+        if itype not in ['l', 'g']:
+            raise ValueError(f"itype {itype} must be either 'l' or 'g'")
+        if rtype not in ['l', 'g']:
+            raise ValueError(f"rtype {rtype} must be either 'l' or 'g'")
+
+        # validate
+        sizes = {'l': self.local_size, 'g': self.global_size}
+        in_range(i, sizes[itype], throws=True)
+
+        # deal with -ve index
+        i = i % sizes[itype]
+
+        # no shift needed
+        if itype == rtype:
+            return i
+        else:
+            if itype == 'l':  # rtype == 'g'
+                i += self.offset
+            elif itype == 'g':  # rtype == 'l'
+                i -= self.offset
+            in_range(i, sizes[rtype], throws=True)
+            return i
+
+    def is_local(self, i, throws=False):
+        '''
+        Is the globally addressed index i owned by this time rank?
+
+        :arg i: globally addressed index.
+        :arg throws: if True, raises IndexError if i is outside the global address range
+        '''
+        try:
+            self.shift_index(i, itype='g', rtype='l')
+            return True
+        except IndexError:
+            if throws:
+                raise
+            else:
+                return False
+
+
+class SharedArray(object):
+    def __init__(self, partition, dtype=None, comm=MPI.COMM_WORLD):
+        '''
+        1D array shared over an MPI comm.
+
+        Each rank has a copy of the entire array of size len(partition) but can only  modify the partition[comm.rank] section of the array.
         Provides method for synchronising the array over the comm and testing if an element is owned by the current rank.
 
-        :arg local_size: number of elements owned by each rank.
+        :arg partition: The number of data elements on each rank. Can be a list of integers, in which case len(partition) must be comm.size. Can be a single integer, in which case all ranks have the same number of elements.
         :arg dtype: datatype, defaults to numpy default dtype.
         :arg comm: MPI communicator the array is distributed over.
         '''
         self.comm = comm
-        self.local_size = local_size
-        self.global_size = comm.size*local_size
         self.rank = comm.rank
+        self.layout = DistributedDataLayout(partition, comm=comm)
+        self.partition = partition
+        self.local_size = self.layout.local_size
+        self.global_size = self.layout.global_size
+        self.offset = self.layout.offset
 
-        self._data = zero_array(self.global_size, dtype=dtype)
-        self.offset = self.rank*self.local_size
+        self._data = zeros(self.global_size, dtype=dtype)
 
         self.dglobal = self._GlobalAccessor(self)
         self.dlocal = self._LocalAccessor(self)
 
     class _GlobalAccessor(object):
         '''
-        Manages access by global addressing
+        Manage access by global addressing
         '''
         def __init__(self, parent):
             self.parent = parent
+            self.layout = parent.layout
 
         def __getitem__(self, i):
             return self.parent._data[i]
 
         def __setitem__(self, i, val):
-            i = i % self.parent.global_size
-            if not self.parent.is_owned(i):
-                raise IndexError(f"Global index {i} is not on rank {self.parent.rank}")
+            self.layout.is_local(i, throws=True)
             self.parent._data[i] = val
 
     class _LocalAccessor(object):
         '''
-        Manages access by local addressing
+        Manage access by local addressing
         '''
         def __init__(self, parent):
             self.parent = parent
-
-        def _validate_index(self, i):
-            i = i % self.parent.local_size
-            global_index = self.parent.offset + i
-            if not self.parent.is_owned(global_index):
-                raise IndexError(f"Local index {i} is not owned by rank {self.parent.rank}")
-            return global_index
+            self.layout = parent.layout
 
         def __getitem__(self, i):
-            global_index = self._validate_index(i)
-            return self.parent._data[global_index]
+            i = self.layout.shift_index(i, itype='l', rtype='g')
+            return self.parent._data[i]
 
         def __setitem__(self, i, val):
-            global_index = self._validate_index(i)
-            self.parent._data[global_index] = val
-
-    def is_owned(self, i):
-        '''
-        Is the index i owned by this time rank?
-        '''
-        return self.offset <= i < self.offset + self.local_size
+            i = self.layout.shift_index(i, itype='l', rtype='g')
+            self.parent._data[i] = val
 
     def synchronise(self):
         """
-        Synchronise the array over the comm
+        Synchronise the array over the comm.
 
-        Until this method is called, array elements not owned by the current rank are not guaranteed to be vald
+        Until this method is called, array elements not owned by the current rank are not guaranteed to be valid.
         """
-        self.comm.Allgather(MPI.IN_PLACE, self._data)
+        offsets = zeros(self.comm.size)
+        offsets[1:] = cumsum(self.partition)[:-1]
+        self.comm.Allgatherv(MPI.IN_PLACE, [self._data, self.partition])
--- tests/test_parallel_arrays.py
+++ tests/test_parallel_arrays.py
@@ -1,18 +1,127 @@
 import pytest
-# from pyop2.mpi import MPI
-from asQ import DistributedArray
+from pyop2.mpi import MPI
+from asQ import DistributedDataLayout, SharedArray
 
 
 @pytest.mark.parallel(nprocs=4)
-def test_distributed_array():
-    array = DistributedArray(local_size=4, dtype=int)
+@pytest.mark.parametrize("partition", [3, [2, 3, 4, 2]])
+def test_distributed_data_layout(partition):
+    comm = MPI.COMM_WORLD
+    layout = DistributedDataLayout(partition, comm=comm)
+
+    if isinstance(partition, int):
+        partition = [partition for _ in range(layout.comm.size)]
+
+    rank = layout.rank
+    nlocal = partition[rank]
+    nglobal = sum(partition)
+    offset = sum(partition[:rank])
+
+    # check attributes
+    assert layout.partition == partition
+    assert layout.rank == comm.rank
+    assert layout.local_size == nlocal
+    assert layout.global_size == nglobal
+    assert layout.offset == offset
+
+    max_indices = {
+        'l': nlocal,
+        'g': nglobal
+    }
+
+    # shift index: from_range == to_range
+    for itype in max_indices.keys():
+
+        imax = max_indices[itype]
+        i = imax - 1
+
+        # +ve index unchanged
+        pos_shift = layout.shift_index(i, itype=itype, rtype=itype)
+        assert (pos_shift == i)
+
+        # -ve index changed to +ve
+        neg_shift = layout.shift_index(-i, itype=itype, rtype=itype)
+        assert (neg_shift == imax - i)
+
+    # local address -> global address
+
+    ilocal = 1
+
+    # +ve index in local range
+    iglobal = layout.shift_index(ilocal, itype='l', rtype='g')
+    assert (iglobal == offset + ilocal)
+
+    # -ve index in local range
+    iglobal = layout.shift_index(-ilocal, itype='l', rtype='g')
+    assert (iglobal == offset + nlocal - ilocal)
+
+    ilocal = nlocal + 1
+
+    # +ve index out of local range
+    with pytest.raises(IndexError):
+        iglobal = layout.shift_index(ilocal, itype='l', rtype='g')
+
+    # -ve index out of local range
+    with pytest.raises(IndexError):
+        iglobal = layout.shift_index(-ilocal, itype='l', rtype='g')
+
+    # global address -> local address
+
+    # +ve index in range
+    iglobal = offset + 1
+    ilocal = layout.shift_index(iglobal, itype='g', rtype='l')
+    assert (ilocal == 1)
+
+    assert layout.is_local(iglobal)
+
+    # -ve index in range
+    iglobal = -sum(partition) + offset + 1
+    ilocal = layout.shift_index(iglobal, itype='g', rtype='l')
+    assert (ilocal == 1)
+
+    assert layout.is_local(iglobal)
+
+    # +ve index out of local range
+    iglobal = (offset + nlocal + 1) % nglobal
+    with pytest.raises(IndexError):
+        ilocal = layout.shift_index(iglobal, itype='g', rtype='l')
+
+    assert not layout.is_local(iglobal)
+
+    with pytest.raises(IndexError):
+        layout.is_local(iglobal, throws=True)
+
+    # -ve index out of local range
+    iglobal = -(nglobal - (offset + nlocal))
+    with pytest.raises(IndexError):
+        ilocal = layout.shift_index(iglobal, itype='g', rtype='l')
+
+    assert not layout.is_local(iglobal)
+
+    with pytest.raises(IndexError):
+        layout.is_local(iglobal, throws=True)
+
+
+@pytest.mark.parallel(nprocs=4)
+@pytest.mark.parametrize("partition", [3, [2, 3, 4, 2]])
+def test_shared_array(partition):
+    comm = MPI.COMM_WORLD
+    rank = comm.rank
+
+    if isinstance(partition, int):
+        partition = [partition for _ in range(comm.size)]
+
+    array = SharedArray(partition=partition, dtype=int, comm=comm)
+
+    local_size = partition[rank]
+    offset = sum(partition[:rank])
 
     # check everything is zero to start
     for i in range(array.global_size):
         assert array.dglobal[i] == 0
 
-    # each rank sets its own elements
-    for i in range(array.local_size):
+    # each rank sets its own elements using local access
+    for i in range(local_size):
         array.dlocal[i] = array.rank + 1
 
     # synchronise
@@ -21,7 +130,70 @@ def test_distributed_array():
     # check all elements are correct
     for rank in range(array.comm.size):
         check = rank + 1
-        offset = rank*array.local_size
-        for i in range(array.local_size):
+        offset = sum(partition[:rank])
+        for i in range(partition[rank]):
             j = offset + i
             assert array.dglobal[j] == check
+
+    # each rank sets its own elements using global access
+    for i in range(local_size):
+        j = array.offset + i
+        array.dglobal[j] = (array.rank + 1)*2
+
+    array.synchronise()
+
+    # check all elements are correct
+    for i in range(local_size):
+        assert array.dlocal[i] == (array.rank + 1)*2
+
+    for rank in range(array.comm.size):
+        check = (rank + 1)*2
+        offset = sum(partition[:rank])
+        for i in range(partition[rank]):
+            j = offset + i
+            assert array.dglobal[j] == check
+
+
+@pytest.mark.skip
+@pytest.mark.parallel(nprocs=4)
+@pytest.mark.parametrize("partition", [3])
+def test_shared_array_manager(partition):
+    comm = MPI.COMM_WORLD
+    rank = comm.rank
+
+    if isinstance(partition, int):
+        partition = [partition for _ in range(comm.size)]
+
+    # local_size = partition[rank]
+
+    array = SharedArray(partition=partition, dtype=int, comm=comm)
+
+    # try to set a globally addressed element we don't own
+    bad_global_index = array.offset-1
+
+    # with pytest.raises(IndexError):
+    #     array.dglobal[bad_global_index] = 1
+
+    try:
+        array.dglobal[bad_global_index] = 1
+        assert False, 'should have thrown'
+    except IndexError as err:
+        print(f"IndexError on rank {rank}: "+str(err))
+        pass
+    except Exception as err:
+        # print("unidentified error")
+        print(f"Exception on rank {rank}: "+str(err))
+        pass
+
+    # with pytest.raises(IndexError):
+    #     array.dglobal[bad_global_index] = 1
+
+    # # try to set a locally addressed element we don't own
+    # bad_local_index = local_size
+
+    # with pytest.raises(IndexError):
+    #     array.dlocal[bad_local_index] = 1
+
+    # # try to get a locally addressed element we don't own
+    # with pytest.raises(IndexError):
+    #     x = array.dlocal[bad_local_index]  # noqa: F841 unused variable
