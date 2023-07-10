0c5987194b5769c6d759e836c21818d1f0f8ee90
Abdalaziz <a.hamdan@imperial.ac.uk>
2023-07-10 12:01:08 +0100

    add time to form_function

--- examples/shallow_water/williamson2.py
+++ examples/shallow_water/williamson2.py
@@ -80,7 +80,7 @@ hn.assign(H + etan - b)
 # nonlinear swe forms
 
 def form_function(u, h, v, q, t):
-    return swe.nonlinear.form_function(mesh, earth.Gravity, b, f, u, h, v, q)
+    return swe.nonlinear.form_function(mesh, earth.Gravity, b, f, u, h, v, q, t)
 
 
 def form_mass(u, h, v, q):
@@ -88,7 +88,7 @@ def form_mass(u, h, v, q):
 
 
 def linear_function(u, h, v, q, t):
-    return swe.linear.form_function(mesh, earth.Gravity, H, f, u, h, v, q)
+    return swe.linear.form_function(mesh, earth.Gravity, H, f, u, h, v, q, t)
 
 
 def linear_mass(u, h, v, q):
