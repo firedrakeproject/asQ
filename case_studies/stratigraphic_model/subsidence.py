e302648f2222e9575d90d2a84d2a7367c1c6c2f0
Abdalaziz <a.hamdan@imperial.ac.uk>
2023-10-04 13:27:12 +0100

    separating the keep-up and the subsidence scenarios, improving the initial guess(slice_length=1)

--- case_studies/stratigraphic_model/subsidence.py
+++ case_studies/stratigraphic_model/subsidence.py
@@ -1,21 +1,20 @@
-import matplotlib.pyplot as plt
 from math import pi
-from matplotlib.animation import FuncAnimation
 import firedrake as fd
 from firedrake.petsc import PETSc
 import asQ
+from utils.serial import SerialMiniApp
 
 import argparse
 
 parser = argparse.ArgumentParser(
     description='Paradiag for Stratigraphic model that simulate formation of sedimentary rock over geological time.',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--nx', type=int, default=200, help='Number of cells along each square side.')
+parser.add_argument('--nx', type=int, default=500, help='Number of cells along each square side.')
 parser.add_argument('--degree', type=int, default=1, help='Degree of the scalar and velocity spaces.')
 parser.add_argument('--theta', type=float, default=0.5, help='Parameter for the implicit theta timestepping method.')
 parser.add_argument('--nwindows', type=int, default=1, help='Number of time-windows.')
 parser.add_argument('--nslices', type=int, default=2, help='Number of time-slices per time-window.')
-parser.add_argument('--slice_length', type=int, default=2, help='Number of timesteps per time-slice.')
+parser.add_argument('--slice_length', type=int, default=1, help='Number of timesteps per time-slice.')
 parser.add_argument('--alpha', type=float, default=0.0001, help='Circulant coefficient.')
 parser.add_argument('--nsample', type=int, default=16, help='Number of sample points for plotting.')
 parser.add_argument('--show_args', action='store_true', help='Output all the arguments.')
@@ -32,7 +31,7 @@ time_partition = tuple(args.slice_length for _ in range(args.nslices))
 window_length = sum(time_partition)
 nsteps = args.nwindows*window_length
 
-dt = 1000
+dt = 200
 
 # The Ensemble with the spatial and time communicators
 ensemble = asQ.create_ensemble(time_partition)
@@ -40,7 +39,7 @@ ensemble = asQ.create_ensemble(time_partition)
 # # # === --- domain --- === # # #
 
 # The mesh needs to be created with the spatial communicator
-mesh = fd.SquareMesh(args.nx, args.nx, 100, quadrilateral=False, comm=ensemble.comm)
+mesh = fd.SquareMesh(args.nx, args.nx, 100000, quadrilateral=False, comm=ensemble.comm)
 
 V = fd.FunctionSpace(mesh, "CG", args.degree)
 
@@ -62,25 +61,46 @@ def L(G_0, d):
     return G_0*fd.conditional(d > 0, fd.exp(-d/10)/(1 + fd.exp(-50*d)), fd.exp((50-1/10)*d)/(fd.exp(50*d) + 1))
 
 
+def Subsidence(t):
+    return -((100000-x)/10000)*(t/100000)
+
+
+def sea_level(A, t):
+    return A*fd.sin(2*pi*t/500000)
+
+
 # # # === --- finite element forms --- === # # #
-
-
 def form_mass(s, q):
-    return s*q*fd.dx
+    return (s*q*fd.dx)
 
 
 D_c = fd.Constant(.002)
 G_0 = fd.Constant(.004)
 A = fd.Constant(50)
-b = 100*fd.tanh(1/20000*(x-50000))
+b = 100*fd.tanh(1/100000*(x-50000))
 
 
 def form_function(s, q, t):
-    return D(D_c, A*fd.sin(2*pi*t/500000)-b-s)*fd.inner(fd.grad(s), fd.grad(q))*fd.dx-L(G_0, A*fd.sin(2*pi*t/500000)-b-s)*q*fd.dx
+    return (D(D_c, sea_level(A, t)-b-Subsidence(t)-s)*fd.inner(fd.grad(s), fd.grad(q))*fd.dx-L(G_0, sea_level(A, t)-b-Subsidence(t)-s)*q*fd.dx)
 
 
 # # # === --- PETSc solver parameters --- === # # #
 
+# Solver parameters for the serial app
+
+sp = {
+    #    "snes_atol": 1.0e-8,
+    #    "snes_rtol": 1.0e-8,
+    "snes_max_it": 4,
+    "snes_convergence_test": "skip",
+    "snes_monitor": None,
+    "snes_linesearch_type": "l2",
+    "snes_converged_reason": None,
+    "mat_type": "aij",
+    "ksp_type": "preonly",
+    "pc_type": "lu",
+    "pc_factor_mat_solver_type": "mumps"
+}
 
 # The PETSc solver parameters used to solve the
 # blocks in step (b) of inverting the ParaDiag matrix.
@@ -147,26 +167,45 @@ pdg = asQ.Paradiag(ensemble=ensemble,
 
 # This is a callback which will be called before pdg solves each time-window
 # We can use this to make the output a bit easier to read
+
+ic = fd.Function(V)
+ic.assign(s0)
+
+
 def window_preproc(pdg, wndw, rhs):
     PETSc.Sys.Print('')
     PETSc.Sys.Print(f'### === --- Calculating time-window {wndw} --- === ###')
     PETSc.Sys.Print('')
+    aaofunc = pdg.aaofunc
+    Dt = (1 + aaofunc.transform_index(0, from_range='slice', to_range='window'))*dt
+    miniapp = SerialMiniApp(dt, args.theta,
+                            ic,
+                            form_mass,
+                            form_function,
+                            sp)
 
+    miniapp.time.assign(fd.Constant(Dt + wndw*(window_length)*dt))
+    miniapp.solve(1)
+    aaofunc.set_field(0, miniapp.w1, index_range='slice')
 
 # The last time-slice will be saving snapshots to create an animation.
 # The layout member describes the time_partition.
 # layout.is_local(i) returns True/False if the timestep index i is on the
 # current time-slice. Here we use -1 to mean the last timestep in the window.
+
+
 is_last_slice = pdg.layout.is_local(-1)
+
 
 # Make an output Function on the last time-slice and start a snapshot list
 if is_last_slice:
     qout = fd.Function(V)
     timeseries = [s0.copy(deepcopy=True)]
 
-
 # This is a callback which will be called after pdg solves each time-window
 # We can use this to save the last timestep of each window for plotting.
+
+
 def window_postproc(pdg, wndw, rhs):
     if is_last_slice:
         # The aaos is the AllAtOnceSystem which represents the time-dependent problem.
@@ -174,6 +213,7 @@ def window_postproc(pdg, wndw, rhs):
         # timestep and place it in qout.
         pdg.aaofunc.get_field(-1, index_range='window', uout=qout)
         timeseries.append(qout.copy(deepcopy=True))
+    pdg.aaofunc.bcast_field(-1, ic)
 
 
 # Solve nwindows of the all-at-once system
@@ -201,21 +241,5 @@ PETSc.Sys.Print(f'block linear iterations: {pdg.block_iterations._data}  |  iter
 
 # We can write these diagnostics to file, along with some other useful information.
 # Files written are: aaos_metrics.txt, block_metrics.txt, paradiag_setup.txt, solver_parameters.txt
+
 asQ.write_paradiag_metrics(pdg)
-
-# Make an animation from the snapshots we collected and save it to periodic.mp4.
-if is_last_slice:
-
-    fn_plotter = fd.FunctionPlotter(mesh, num_sample_points=args.nsample)
-
-    fig, axes = plt.subplots()
-    axes.set_aspect('equal')
-    colors = fd.tripcolor(qout, num_sample_points=args.nsample, axes=axes)
-    fig.colorbar(colors)
-
-    def animate(q):
-        colors.set_array(fn_plotter(q))
-
-    animation = FuncAnimation(fig, animate, frames=timeseries)
-
-    animation.save("periodic.mp4", writer="ffmpeg")
