99f9a7e97266ef64e4f363ac9b8a0342a3914f1a
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2023-10-24 12:19:57 +0100

    straka setup utils

--- case_studies/vertical_slice/straka_serial.py
+++ case_studies/vertical_slice/straka_serial.py
@@ -3,6 +3,7 @@ from firedrake.petsc import PETSc
 from pyop2.mpi import MPI
 from utils.diagnostics import convective_cfl_calculator
 from utils.serial import SerialMiniApp
+from utils.vertical_slice import straka
 from utils import compressible_flow as euler
 
 import argparse
@@ -26,22 +27,10 @@ PETSc.Sys.Print("Setting up problem")
 
 comm = fd.COMM_WORLD
 
-nlayers = args.nlayers
-base_columns = args.ncolumns
-dt = args.dt
-L = 51200.
-distribution_parameters = {"partition": True, "overlap_type": (fd.DistributedMeshOverlapType.VERTEX, 2)}
-m = fd.PeriodicIntervalMesh(base_columns, L,
-                            distribution_parameters=distribution_parameters,
-                            comm=comm)
-# translate the mesh to the left by 51200/25600
-m.coordinates.dat.data[:] -= 25600
-
-# build volume mesh
-H = 6400.  # Height position of the model top
-mesh = fd.ExtrudedMesh(m, layers=nlayers, layer_height=H/nlayers)
+mesh = straka.mesh(comm, ncolumns=args.ncolumns, nlayers=args.nlayers)
 n = fd.FacetNormal(mesh)
 
+dt = args.dt
 gas = euler.StandardAtmosphere(N=0.01)
 
 W, Vv = euler.function_space(mesh, horizontal_degree=args.degree,
@@ -52,42 +41,7 @@ V1, V2, Vt = W.subfunctions  # velocity, density, temperature
 PETSc.Sys.Print(f"DoFs: {W.dim()}")
 PETSc.Sys.Print(f"DoFs/core: {W.dim()/mesh.comm.size}")
 
-Un = fd.Function(W)
-
-x, z = fd.SpatialCoordinate(mesh)
-
-# N^2 = (g/theta)dtheta/dz => dtheta/dz = theta N^2g => theta=theta_0exp(N^2gz)
-Tsurf = fd.Constant(300.)
-thetab = Tsurf
-
-Up = fd.as_vector([fd.Constant(0.0), fd.Constant(1.0)])  # up direction
-
-un, rhon, thetan = Un.subfunctions
-thetan.interpolate(thetab)
-theta_back = fd.Function(Vt).assign(thetan)
-rhon.assign(1.0e-5)
-
-euler.hydrostatic_rho(Vv, V2, mesh, thetan, rhon,
-                      pi_boundary=fd.Constant(1.0),
-                      gas=gas, Up=Up, top=False)
-
-x = fd.SpatialCoordinate(mesh)
-xc = 0.
-xr = 4000.
-zc = 3000.
-zr = 2000.
-r = fd.sqrt(((x[0]-xc)/xr)**2 + ((x[1]-zc)/zr)**2)
-T_pert = fd.conditional(r > 1., 0., -7.5*(1.+fd.cos(fd.pi*r)))
-# T = theta*Pi so Delta theta = Delta T/Pi assuming Pi fixed
-
-Pi_back = euler.pi_formula(rhon, thetan, gas)
-# this keeps perturbation at zero away from bubble
-thetan.project(theta_back + T_pert/Pi_back)
-# save the background stratification for rho
-rho_back = fd.Function(V2).assign(rhon)
-# Compute the new rho
-# using rho*theta = Pi which should be held fixed
-rhon.project(rhon*thetan/theta_back)
+Un, rho_back, theta_back = straka.initial_conditions(mesh, W, Vv, gas)
 
 # The timestepping forms
 
@@ -95,14 +49,12 @@ viscosity = fd.Constant(75.)
 
 form_mass = euler.get_form_mass()
 
+up = fd.as_vector([fd.Constant(0.0), fd.Constant(1.0)])  # up direction
 form_function = euler.get_form_function(
-    n=n, Up=Up, c_pen=fd.Constant(2.0**(-7./2)),
+    n=n, Up=up, c_pen=fd.Constant(2.0**(-7./2)),
     gas=gas, mu=None, viscosity=viscosity, diffusivity=viscosity)
 
-zv = fd.as_vector([fd.Constant(0.), fd.Constant(0.)])
-bcs = [fd.DirichletBC(W.sub(0), zv, "bottom"),
-       fd.DirichletBC(W.sub(0), zv, "top")]
-
+bcs = straka.boundary_conditions(W)
 for bc in bcs:
     bc.apply(Un)
 
--- utils/vertical_slice/__init__.py
+++ utils/vertical_slice/__init__.py
@@ -1,2 +1,2 @@
-from utils.vertical_slice.vertical_slice import *  # noqa:F401
 from utils.vertical_slice.mount_agnesi import *  # noqa:F401
+from utils.vertical_slice.straka import *  # noqa:F401
