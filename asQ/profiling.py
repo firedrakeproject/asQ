c595ba3243a6a2d46cec67cfa611143548fae678
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2023-12-15 10:55:38 +0000

    Decorator to call MPI_Barrier before any collective operations. This prevents load imbalance from showing up as a long time spent in the collective operation.

--- asQ/profiling.py
+++ asQ/profiling.py
@@ -1,10 +1,25 @@
 import os
+from functools import wraps, partial
 
-profile_type = os.getenv("ASQ_PROFILE")
+__all__ = ['profile_type', 'profiler', 'coll_barrier']
 
-# default is to profile timing
-if profile_type is None:
-    profile_type = "PETSC_LOG"
+_default_profile_type = "PETSC_LOG"
+
+profile_type = os.getenv("ASQ_PROFILE", _default_profile_type)
+
+COLL_BARRIER = os.getenv("ASQ_COLL_BARRIER")
+
+
+def pass_through(func):
+    @wraps(func)
+    def pass_through(*args, **kwargs):
+        return func(*args, **kwargs)
+    return pass_through
+
+
+# # #
+# Set profiling decorator
+# # #
 
 # profile timing
 if profile_type == "PETSC_LOG":
@@ -24,7 +39,6 @@ elif profile_type == "MEMORY":
 elif profile_type == "MEMORY_FILE":
     from memory_profiler import profile
     from mpi4py import MPI
-    from functools import partial
     rank = MPI.COMM_WORLD.rank
     mprof_file = open(f"asq_memprof_log{rank}.dat", "w")
     prof = partial(profile, stream=mprof_file)
@@ -35,9 +49,7 @@ elif profile_type == "MEMORY_FILE":
 # do not profile
 elif profile_type == "NONE":
     def prof(func):
-        def pass_through(*args, **kwargs):
-            return func(*args, **kwargs)
-        return pass_through
+        return pass_through(func)
 
     def profiler():
         return prof
@@ -48,9 +60,23 @@ else:
     warnings.warn(f"Unknown profile type ASQ_PROFILE={profile_type}. Defaulting to no profiling.")
 
     def prof(func):
-        def pass_through(*args, **kwargs):
-            return func(*args, **kwargs)
-        return pass_through
+        return pass_through(func)
 
     def profiler():
         return prof
+
+# # #
+# Set collective communications barrier
+# # #
+
+if COLL_BARRIER is None:
+    def coll_barrier(func):
+        return pass_through(func)
+else:
+    def coll_barrier(func, comm_name='ensemble.ensemble_comm'):
+        @wraps(func)
+        def coll_barrier_decorator(self, *args, **kwargs):
+            comm = getattr(self, comm_name)
+            comm.Barrier()
+            return func(self, *args, **kwargs)
+        return coll_barrier_decorator
