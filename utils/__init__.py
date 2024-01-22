5ee6ed395fef0108625872705f830e0fe2f4de92
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2024-01-22 10:29:03 +0000

    timing class for multiple solves

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,5 +5,6 @@ import utils.compressible_flow  # noqa: F401
 import utils.vertical_slice  # noqa: F401
 import utils.mg  # noqa: F401
 import utils.diagnostics  # noqa: F401
+import utils.timing  # noqa: F401
 import utils.serial  # noqa: F401
 import utils.misc  # noqa: F401
