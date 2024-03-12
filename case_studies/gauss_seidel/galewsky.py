e24161dbaa34a86edabaa0012a2c8ad3169d6ce5
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2024-03-12 10:21:30 +0000

    switch between serial and parallel gauss-seidel sweeps

--- case_studies/gauss_seidel/galewsky.py
+++ case_studies/gauss_seidel/galewsky.py
@@ -16,27 +16,22 @@ PETSc.Sys.popErrorHandler()
 # get command arguments
 import argparse
 parser = argparse.ArgumentParser(
-    description='Galewsky testcase for ParaDiag solver using fully implicit SWE solver.',
+    description='Galewsky testcase for ParaDiag solver using a pipelined nonlinear Gauss-Seidel method.',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
 
 parser.add_argument('--ref_level', type=int, default=2, help='Refinement level of icosahedral grid.')
 parser.add_argument('--base_level', type=int, default=2, help='Refinement level of coarse grid.')
 parser.add_argument('--nwindows', type=int, default=1, help='Total number of time-windows.')
-parser.add_argument('--nlmethod', type=str, default='gs', choices=['gs', 'jac'], help='Nonlinear method. "gs" for Gauss-Siedel or "jac" for Jacobi.')
 parser.add_argument('--nchunks', type=int, default=4, help='Number of chunks to solve simultaneously.')
 parser.add_argument('--nsweeps', type=int, default=4, help='Number of nonlinear sweeps.')
 parser.add_argument('--nsmooth', type=int, default=1, help='Number of nonlinear iterations per chunk at each sweep.')
-parser.add_argument('--nchecks', type=int, default=1, help='Maximum number of chunks allowed to converge after each sweep.')
-parser.add_argument('--ninitialise', type=int, default=0, help='Number of sweeps before checking convergence.')
 parser.add_argument('--atol', type=float, default=1e5, help='Average atol of each timestep.')
 parser.add_argument('--nslices', type=int, default=2, help='Number of time-slices per time-window.')
 parser.add_argument('--slice_length', type=int, default=2, help='Number of timesteps per time-slice.')
 parser.add_argument('--alpha', type=float, default=1e-4, help='Circulant coefficient.')
 parser.add_argument('--dt', type=float, default=0.5, help='Timestep in hours.')
-parser.add_argument('--filename', type=str, default='galewsky', help='Name of output vtk files')
-parser.add_argument('--metrics_dir', type=str, default='metrics', help='Directory to save paradiag metrics to.')
-parser.add_argument('--print_res', action='store_true', help='Print the residuals of each timestep at each iteration.')
+parser.add_argument('--serial', action='store_true', help='Calculate each chunk in serial.')
 parser.add_argument('--show_args', action='store_true', help='Output all the arguments.')
 
 args = parser.parse_known_args()
@@ -117,7 +112,7 @@ aux_pc = {
 
 sparameters = {
     'mat_type': 'matfree',
-    'ksp_type': 'fgmres',
+    'ksp_type': 'gmres',
     'ksp': {
         'atol': 1e-5,
         'rtol': 1e-5,
@@ -154,7 +149,7 @@ sparameters_diag = {
         'atol': patol,
     },
     'pc_type': 'python',
-    'pc_python_type': 'asQ.DiagFFTPC',
+    'pc_python_type': 'asQ.CirculantPC',
     'diagfft_alpha': args.alpha,
     'diagfft_state': 'window',
     'aaos_jacobian_state': 'current'
@@ -187,8 +182,8 @@ chunk_solver = asQ.AllAtOnceSolver(chunk_aaoform, chunk_aaofunc,
                                    solver_parameters=sparameters_diag,
                                    appctx=appctx)
 
-# which chunk is holding which part of the total timeseries?
-chunk_indexes = np.array([i for i in range(args.nchunks)], dtype=int)
+# which chunk_id is holding which part of the total timeseries?
+chunk_indexes = np.array([*range(args.nchunks)], dtype=int)
 
 # we need to make this an array so we can send it via mpi
 convergence_flag = np.array([False], dtype=bool)
@@ -196,9 +191,8 @@ convergence_flag = np.array([False], dtype=bool)
 # first mpi rank on each chunk (assumes all chunks are equal size):
 chunk_root = lambda c: c*chunk_ensemble.global_comm.size
 
-# which chunks are currently at the beginning/end of the sweep?
-first_chunk = 0
-last_chunk = args.nchunks - 1
+# am I the chunk with the earliest timesteps?
+earliest = lambda: (chunk_id == chunk_indexes[0])
 
 # update chunk ics from previous chunk
 uprev = fd.Function(W)
@@ -214,15 +208,15 @@ def update_chunk_halos(uhalo):
     # ring communication so the first chunk can
     # pick up after last chunk after convergence
 
-    # send forward
+    # send forward last step of chunk
     if chunk_end:
-        dst = (global_rank + 1) % global_size
-        global_ensemble.send(chunk_aaofunc[-1], dest=dst, tag=dst)
+        dest = (global_rank + 1) % global_size
+        global_ensemble.send(chunk_aaofunc[-1], dest=dest, tag=dest)
 
-    # recv previous
+    # recv updated ics from previous chunk
     if chunk_begin:
-        src = (global_rank - 1) % global_size
-        global_ensemble.recv(uhalo, source=src, tag=global_rank)
+        source = (global_rank - 1) % global_size
+        global_ensemble.recv(uhalo, source=source, tag=global_rank)
 
     # broadcast new ics to all ranks
     chunk_ensemble.bcast(uhalo)
@@ -235,33 +229,37 @@ nconverged = 0
 
 PETSc.Sys.Print('### === --- Initialising all chunks --- === ###')
 PETSc.Sys.Print('')
-sleep_time = 0.1
+sleep_time = 0.01
 for j in range(args.nchunks):
     PETSc.Sys.Print(f'    === --- Initial nonlinear sweep {j} --- ===    ')
     PETSc.Sys.Print('')
 
     # only smooth chunks that the first sweep has reached
-    # for i in range(j+1):
-    #     PETSc.Sys.Print(f'        --- Calculating chunk {i} ---        ')
 
-    #     global_comm.Barrier(); sleep(sleep_time)
-    #     if chunk_id == i:
-    #         chunk_solver.solve()
-    #     global_comm.Barrier(); sleep(sleep_time)
+    if args.serial:
+        for i in range(j+1):
+            PETSc.Sys.Print(f'        --- Calculating chunk {i} ---        ')
 
-    #     PETSc.Sys.Print("")
+            global_comm.Barrier()
+            sleep(sleep_time)
+            if chunk_id == i:
+                chunk_solver.solve()
+            global_comm.Barrier()
+            sleep(sleep_time)
 
-    if chunk_id < j+1:
-        chunk_solver.solve()
+            PETSc.Sys.Print("")
+    else:
+        if chunk_id < j+1:
+            chunk_solver.solve()
 
     # propogate solution
     update_chunk_halos(uprev)
 
-    # update initial conditions
+    # update initial condition guess for later chunks
     if chunk_id != 0:
         chunk_aaofunc.initial_condition.assign(uprev)
 
-    # initial guess before first sweep is persistence forecast
+    # initial guess in front of first sweep is persistence forecast
     if chunk_id > j:
         chunk_aaofunc.assign(chunk_aaofunc.initial_condition)
 
@@ -273,29 +271,32 @@ for j in range(args.nsweeps):
     PETSc.Sys.Print('')
 
     # 1) one smoothing application on each chunk
-    # for i in range(args.nchunks):
-    #     PETSc.Sys.Print(f'        --- Calculating chunk {i} on solver {chunk_indexes[i]} ---        ')
+    if args.serial:
+        for i in range(args.nchunks):
+            PETSc.Sys.Print(f'        --- Calculating chunk {i} on solver {chunk_indexes[i]} ---        ')
 
-    #     global_comm.Barrier(); sleep(sleep_time)
-    #     if chunk_id == chunk_indexes[i]:
-    #         chunk_solver.solve()
-    #     global_comm.Barrier(); sleep(sleep_time)
+            global_comm.Barrier()
+            sleep(sleep_time)
+            if chunk_id == chunk_indexes[i]:
+                chunk_solver.solve()
+            global_comm.Barrier()
+            sleep(sleep_time)
 
-    #     PETSc.Sys.Print("")
-    chunk_solver.solve()
+            PETSc.Sys.Print("")
+
+    else:
+        chunk_solver.solve()
 
     # 2) update ics of each chunk from previous chunk
     update_chunk_halos(uprev)
 
-    earliest_chunk = (chunk_id == chunk_indexes[0])
-
-    # everyone uses latest ic guesses except chunk 0 (uses 'exact' ic)
-    if not earliest_chunk:
+    # everyone uses latest ic guesses, except chunk
+    # with earliest timesteps (already has 'exact' ic)
+    if not earliest():
         chunk_aaofunc.initial_condition.assign(uprev)
 
     # 3) check convergence of earliest chunk
-
-    if earliest_chunk:
+    if earliest():
         chunk_aaoform.assemble()
         with chunk_aaoform.F.global_vec_ro() as rvec:
             res = rvec.norm()
@@ -305,28 +306,31 @@ for j in range(args.nsweeps):
     global_ensemble.global_comm.Bcast(convergence_flag,
                                       root=chunk_root(chunk_indexes[0]))
 
-    # 4) shuffle if converged
+    # update and report
     if convergence_flag[0]:
-        # earliest chunk becomes last chunk
-        if earliest_chunk:
-            chunk_aaofunc.assign(uprev)
-
-        # update record of which chunk is in which position
-        for i in range(args.nchunks):
-            chunk_indexes[i] = (chunk_indexes[i] + 1) % args.nchunks
-
         nconverged += 1
 
-    PETSc.Sys.Print('')
-    PETSc.Sys.Print(f">>> Converged chunks: {int(nconverged)}.")
     converged_time = nconverged*chunk_length*args.dt
+    PETSc.Sys.Print('')
+    PETSc.Sys.Print(f">>> Converged chunks: {nconverged}.")
     PETSc.Sys.Print(f">>> Converged time: {converged_time} hours.")
     PETSc.Sys.Print('')
 
+    # 4) stop iterating if we've reached the end
     if nconverged >= args.nwindows:
         PETSc.Sys.Print(f"Finished iterating to {args.nwindows} windows.")
         PETSc.Sys.Print('')
         break
+
+    # 5) shuffle and restart if we haven't reached the end
+    if convergence_flag[0]:
+        # earliest chunk_id becomes last chunk
+        if earliest():
+            chunk_aaofunc.assign(uprev)
+
+        # update record of which chunk_id is in which position
+        for i in range(args.nchunks):
+            chunk_indexes[i] = (chunk_indexes[i] + 1) % args.nchunks
 
 nsweeps = j
 
