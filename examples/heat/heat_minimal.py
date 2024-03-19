e015816a0950531854f2e8a2bf2abdb319eaf4cd
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2024-03-19 10:03:16 +0000

    remove unrelated example edits

--- examples/heat/heat_minimal.py
+++ examples/heat/heat_minimal.py
@@ -1,7 +1,7 @@
 from firedrake import *
 import asQ
 
-time_partition = [2]
+time_partition = [4, 4]
 ensemble = asQ.create_ensemble(time_partition)
 
 mesh = SquareMesh(nx=8, ny=8, L=1,
@@ -22,25 +22,20 @@ def form_function(u, v, t):
 
 
 block_parameters = {
-    'ksp_view': None,
     'ksp_type': 'preonly',
-    'pc_type': 'jacobi'}
+    'pc_type': 'lu'}
 
 solver_parameters = {
     'ksp_monitor': None,
     'ksp_converged_rate': None,
     'snes_type': 'ksponly',
     'mat_type': 'matfree',
-    'ksp_type': 'preonly',
+    'ksp_type': 'gmres',
     'pc_type': 'python',
-    'pc_python_type': 'asQ.CirculantPC',
-    'circulant_alpha': 1e-10,
-    'circulant_block': block_parameters,
-    'diagfft_state': 'linear',
+    'pc_python_type': 'asQ.JacobiPC',
+    'aaojacobi_block': block_parameters,
+    'aaojacobi_state': 'linear',
     'aaos_jacobian_state': 'linear'}
-
-for i in range(sum(time_partition)):
-    solver_parameters[f'diagfft_block_{i}'] = block_parameters
 
 paradiag = asQ.Paradiag(
     ensemble=ensemble,
