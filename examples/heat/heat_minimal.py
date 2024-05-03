a94ae8349d5e62741fa1b2963bdefea43a9645bc
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2024-05-03 17:22:36 +0100

    testing default options

--- examples/heat/heat_minimal.py
+++ examples/heat/heat_minimal.py
@@ -1,7 +1,7 @@
 from firedrake import *
 import asQ
 
-time_partition = [4, 4]
+time_partition = [2, 2]
 ensemble = asQ.create_ensemble(time_partition)
 
 mesh = SquareMesh(nx=8, ny=8, L=1,
@@ -23,19 +23,68 @@ def form_function(u, v, t):
 
 block_parameters = {
     'ksp_type': 'preonly',
-    'pc_type': 'lu'}
+    'pc_type': 'ilu',
+    'ksp_view': None,
+    'pc_factor_mat_solver_type': 'petsc'}
 
 solver_parameters = {
     'ksp_monitor': None,
     'ksp_converged_rate': None,
     'snes_type': 'ksponly',
     'mat_type': 'matfree',
-    'ksp_type': 'gmres',
+    'ksp_type': 'preonly',
     'pc_type': 'python',
-    'pc_python_type': 'asQ.JacobiPC',
-    'aaojacobi_block': block_parameters,
-    'aaojacobi_state': 'linear',
-    'aaos_jacobian_state': 'linear'}
+    # 'pc_python_type': 'asQ.JacobiPC',
+    # 'aaojacobi_block': {
+    #     'ksp_type': 'gmres',
+    #     'pc_type': 'ilu',
+    #     'ksp_view': None,
+    #     'pc_factor_mat_solver_type': 'petsc'
+    # },
+    # 'aaojacobi_block_0': {
+    #     'pc_type': 'jacobi',
+    # },
+    # 'aaojacobi_block_1': {
+    #     'ksp_type': 'fgmres',
+    # },
+    'pc_type': 'python',
+    'pc_python_type': 'asQ.SliceJacobiPC',
+    'slice_jacobi_nsteps': 2,
+    'slice_jacobi_slice': {
+        'ksp_type': 'preonly',
+        'pc_type': 'python',
+        'pc_python_type': 'asQ.JacobiPC',
+        'aaojacobi_block': {
+            'ksp_type': 'richardson',
+            'pc_type': 'ilu',
+            'ksp_view': None,
+            'pc_factor_mat_solver_type': 'petsc'
+        },
+        'aaojacobi_block_0': {
+            'pc_type': 'jacobi',
+        },
+        'aaojacobi_block_1': {
+            'ksp_type': 'fgmres',
+        },
+    },
+    'slice_jacobi_slice_1': {
+        'ksp_type': 'preonly',
+        'pc_type': 'python',
+        'pc_python_type': 'asQ.CirculantPC',
+        'circulant_block': {
+            'ksp_type': 'richardson',
+            'pc_type': 'sor',
+            'ksp_view': None,
+            'pc_factor_mat_solver_type': 'petsc'
+        },
+        'circulant_block_0': {
+            'ksp_type': 'fgmres',
+        },
+        'circulant_block_1': {
+            'pc_type': 'jacobi',
+        },
+    }
+}
 
 paradiag = asQ.Paradiag(
     ensemble=ensemble,
