dda01ab3eea824858db89a2b67b8a642c4fff5be
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2024-05-08 10:10:48 +0100

    SliceJacobiPC tests with JacobiPC(nsteps=any) and CirculantPC(nsteps=nt)

--- tests/test_preconditioners.py
+++ tests/test_preconditioners.py
@@ -101,3 +101,155 @@ def test_circulantpc(alpha):
 
     niterations = paradiag.solver.snes.getLinearSolveIterations()
     assert niterations == 3, "CirculantPC should have a convergence rate alpha"
+
+
+nstep = [pytest.param(n, id=f"nstep{n}") for n in (1, 2, 4, 8)]
+
+
+@pytest.mark.parallel(nprocs=8)
+@pytest.mark.parametrize("nsteps", nstep)
+def test_slicejacobipc_jacobi(nsteps):
+    slice_length = 1
+    time_partition = [slice_length for _ in range(8)]
+    ensemble = asQ.create_ensemble(time_partition)
+
+    mesh = fd.UnitSquareMesh(nx=16, ny=16,
+                             comm=ensemble.comm)
+    x, y = fd.SpatialCoordinate(mesh)
+
+    V = fd.FunctionSpace(mesh, "CG", 1)
+    uinitial = fd.Function(V)
+    uinitial.project(fd.sin(x) + fd.cos(y))
+
+    def form_mass(u, v):
+        return u*v*fd.dx
+
+    def form_function(u, v, t):
+        return fd.inner(fd.grad(u), fd.grad(v))*fd.dx
+
+    jacobi_parameters = {
+        'ksp_monitor': None,
+        'ksp_converged_rate': None,
+        'snes_type': 'ksponly',
+        'mat_type': 'matfree',
+        'ksp_type': 'preonly',
+        'pc_type': 'python',
+        'pc_python_type': 'asQ.JacobiPC',
+    }
+
+    slice_parameters = {
+        'ksp_monitor': None,
+        'ksp_converged_rate': None,
+        'snes_type': 'ksponly',
+        'mat_type': 'matfree',
+        'ksp_type': 'preonly',
+        'pc_type': 'python',
+        'pc_python_type': 'asQ.SliceJacobiPC',
+        'slice_jacobi_nsteps': nsteps,
+        'slice_jacobi_slice': {
+            'pc_type': 'python',
+            'pc_python_type': 'asQ.JacobiPC',
+        }
+    }
+
+    paradiag_jacobi = asQ.Paradiag(
+        ensemble=ensemble,
+        form_mass=form_mass,
+        form_function=form_function,
+        ics=uinitial, dt=0.1, theta=0.5,
+        time_partition=time_partition,
+        solver_parameters=jacobi_parameters)
+
+    paradiag_slice = asQ.Paradiag(
+        ensemble=ensemble,
+        form_mass=form_mass,
+        form_function=form_function,
+        ics=uinitial, dt=0.1, theta=0.5,
+        time_partition=time_partition,
+        solver_parameters=slice_parameters)
+
+    paradiag_jacobi.solve(nwindows=1)
+    paradiag_slice.solve(nwindows=1)
+
+    jfunc = paradiag_jacobi.aaofunc
+    sfunc = paradiag_slice.aaofunc
+
+    with jfunc.global_vec_ro() as jvec, sfunc.global_vec_ro() as svec:
+        errvec = jvec - svec
+        err = errvec.norm()
+
+    assert err < 1e-15, "SliceJacobiPC with JacobiPC should be exactly JacobiPC for any slice size"
+
+
+@pytest.mark.parallel(nprocs=4)
+def test_slicejacobipc_circulant():
+    slice_length = 2
+    time_partition = [slice_length for _ in range(4)]
+    ensemble = asQ.create_ensemble(time_partition)
+
+    mesh = fd.UnitSquareMesh(nx=16, ny=16,
+                             comm=ensemble.comm)
+    x, y = fd.SpatialCoordinate(mesh)
+
+    V = fd.FunctionSpace(mesh, "CG", 1)
+    uinitial = fd.Function(V)
+    uinitial.project(fd.sin(x) + fd.cos(y))
+
+    def form_mass(u, v):
+        return u*v*fd.dx
+
+    def form_function(u, v, t):
+        return fd.inner(fd.grad(u), fd.grad(v))*fd.dx
+
+    circulant_parameters = {
+        'ksp_monitor': None,
+        'ksp_converged_rate': None,
+        'snes_type': 'ksponly',
+        'mat_type': 'matfree',
+        'ksp_type': 'preonly',
+        'pc_type': 'python',
+        'pc_python_type': 'asQ.CirculantPC',
+    }
+
+    slice_parameters = {
+        'ksp_monitor': None,
+        'ksp_converged_rate': None,
+        'snes_type': 'ksponly',
+        'mat_type': 'matfree',
+        'ksp_type': 'preonly',
+        'pc_type': 'python',
+        'pc_python_type': 'asQ.SliceJacobiPC',
+        'slice_jacobi_nsteps': sum(time_partition),
+        'slice_jacobi_slice': {
+            'pc_type': 'python',
+            'pc_python_type': 'asQ.CirculantPC',
+        }
+    }
+
+    paradiag_circulant = asQ.Paradiag(
+        ensemble=ensemble,
+        form_mass=form_mass,
+        form_function=form_function,
+        ics=uinitial, dt=0.1, theta=0.5,
+        time_partition=time_partition,
+        solver_parameters=circulant_parameters)
+
+    paradiag_slice = asQ.Paradiag(
+        ensemble=ensemble,
+        form_mass=form_mass,
+        form_function=form_function,
+        ics=uinitial, dt=0.1, theta=0.5,
+        time_partition=time_partition,
+        solver_parameters=slice_parameters)
+
+    paradiag_circulant.solve(nwindows=1)
+    paradiag_slice.solve(nwindows=1)
+
+    cfunc = paradiag_circulant.aaofunc
+    sfunc = paradiag_slice.aaofunc
+
+    with cfunc.global_vec_ro() as cvec, sfunc.global_vec_ro() as svec:
+        errvec = cvec - svec
+        err = errvec.norm()
+
+    assert err < 1e-15, "SliceJacobiPC with CirculantPC should be exactly CirculantPC if nstep=nt"
