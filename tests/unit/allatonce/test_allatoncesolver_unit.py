d413c6097f01ee2ec3cba3a4c463a1f58dcfccef
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2024-11-28 17:05:51 +0000

    reorganise tests into unit and integration

--- tests/unit/allatonce/test_allatoncesolver_unit.py
+++ tests/unit/allatonce/test_allatoncesolver_unit.py
@@ -3,8 +3,9 @@ import firedrake as fd
 import pytest
 
 
-@pytest.mark.parallel(nprocs=6)
-def test_solve_heat_equation_nopc():
+@pytest.mark.parallel(nprocs=2)
+@pytest.mark.parametrize('partition', ['serial', 'parallel'])
+def test_solve_heat_equation_nopc(partition):
     """
     Tests the basic solver setup using the heat equation.
     Solves using unpreconditioned GMRES and checks that the
@@ -13,10 +14,19 @@ def test_solve_heat_equation_nopc():
 
     # set up space-time parallelism
 
-    nslices = fd.COMM_WORLD.size//2
-    slice_length = 2
+    window_length = 6
 
-    time_partition = tuple((slice_length for _ in range(nslices)))
+    nprocs = fd.COMM_WORLD.size
+    assert window_length % nprocs == 0, "test setup incorrectly"
+
+    if partition == 'serial':
+        time_partition = window_length
+    elif partition == 'parallel':
+        slice_length = window_length//nprocs
+        time_partition = [slice_length for _ in range(nprocs)]
+    else:
+        assert False, "Unrecognised partition type"
+
     ensemble = asQ.create_ensemble(time_partition, comm=fd.COMM_WORLD)
 
     mesh = fd.UnitSquareMesh(6, 6, comm=ensemble.comm)
@@ -78,95 +88,13 @@ def test_solve_heat_equation_nopc():
     assert residual < atol, "GMRES should converge to prescribed tolerance even without preconditioning"
 
 
-def test_solve_heat_equation_circulantpc():
-    """
-    Tests the basic solver setup using the heat equation.
-    Solves using GMRES preconditioned with the CirculantPC
-    and checks that the residual of the all-at-once-form
-    is below tolerance.
-    """
-
-    # set up space-time parallelism
-
-    window_length = 4
-
-    time_partition = window_length
-    ensemble = asQ.create_ensemble(time_partition, comm=fd.COMM_WORLD)
-
-    mesh = fd.UnitSquareMesh(6, 6, comm=ensemble.comm)
-    V = fd.FunctionSpace(mesh, "CG", 1)
-
-    # all-at-once function and initial conditions
-
-    x, y = fd.SpatialCoordinate(mesh)
-    ics = fd.Function(V).interpolate(fd.exp(-((x - 0.5)**2 + (y - 0.5)**2) / 0.5**2))
-
-    aaofunc = asQ.AllAtOnceFunction(ensemble, time_partition, V)
-    aaofunc.assign(ics)
-
-    # all-at-once form
-
-    dt = 0.01
-    theta = 1.0
-
-    def form_function(u, v, t):
-        return fd.inner(fd.grad(u), fd.grad(v))*fd.dx
-
-    def form_mass(u, v):
-        return fd.inner(u, v)*fd.dx
-
-    aaoform = asQ.AllAtOnceForm(aaofunc, dt, theta,
-                                form_mass, form_function)
-
-    # solver and options
-
-    atol = 1.0e-8
-    solver_parameters = {
-        'snes_type': 'ksponly',
-        'snes': {
-            'monitor': None,
-            'converged_reason': None,
-            'atol': atol,
-            'rtol': 1.0e-100,
-            'stol': 1.0e-100,
-        },
-        'ksp_type': 'gmres',
-        'mat_type': 'matfree',
-        'ksp': {
-            'monitor': None,
-            'converged_rate': None,
-            'atol': atol,
-            'rtol': 1.0e-100,
-            'stol': 1.0e-100,
-        },
-        'pc_type': 'python',
-        'pc_python_type': 'asQ.CirculantPC',
-    }
-
-    aaosolver = asQ.AllAtOnceSolver(aaoform, aaofunc,
-                                    solver_parameters=solver_parameters)
-
-    aaosolver.solve()
-
-    # check residual
-
-    aaoform.assemble(func=aaofunc)
-    residual = fd.norm(aaoform.F.function)
-
-    assert residual < atol, "GMRES should converge to prescribed tolerance with CirculantPC"
-
-
 extruded = [pytest.param(False, id="standard_mesh"),
             pytest.param(True, id="extruded_mesh")]
-
-cpx_types = [pytest.param('vector', id="vector_cpx"),
-             pytest.param('mixed', id="mixed_cpx")]
 
 
 @pytest.mark.parallel(nprocs=4)
 @pytest.mark.parametrize("extrude", extruded)
-@pytest.mark.parametrize("cpx_type", cpx_types)
-def test_solve_mixed_wave_equation(extrude, cpx_type):
+def test_solve_mixed_wave_equation_nopc(extrude):
     """
     Tests the solver setup using a nonlinear wave equation.
     Solves using GMRES preconditioned with CirculantPC and checks
@@ -249,20 +177,13 @@ def test_solve_mixed_wave_equation(extrude, cpx_type):
             'stol': 1e-100,
         },
         'mat_type': 'matfree',
-        'ksp_type': 'preonly',
+        'pc_type': 'none',
+        'ksp_type': 'gmres',
         'ksp': {
             'monitor': None,
-            'converged_reason': None,
+            'converged_rate': None,
+            'rtol': 1e-3,
         },
-        'pc_type': 'python',
-        'pc_python_type': 'asQ.CirculantPC',
-        'circulant_alpha': 1e-3,
-        'circulant_block': {
-            'ksp_type': "preonly",
-            'pc_type': 'lu',
-            'pc_factor_mat_solver_type': 'mumps'
-        },
-        'circulant_complex_proxy': cpx_type
     }
 
     aaosolver = asQ.AllAtOnceSolver(aaoform, aaofunc,
