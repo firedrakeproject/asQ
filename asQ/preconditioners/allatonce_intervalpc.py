b55525fbc32b89c03870b716df067406dbe46253
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2025-04-02 19:10:23 +0100

    tidy of IntervalJacobiPC and factoring out shared base class for IntervalJacobiPC and IntervalGaussSeidelPC

--- asQ/preconditioners/allatonce_intervalpc.py
+++ asQ/preconditioners/allatonce_intervalpc.py
@@ -6,34 +6,10 @@ from asQ.allatonce import (
 from asQ.preconditioners.base import (
     AllAtOncePCBase, get_default_options)
 
-__all__ = ("SliceJacobiPC", "SliceGaussSeidelPC")
+__all__ = ("IntervalJacobiPC", "IntervalGaussSeidelPC")
 
 
-class SliceJacobiPC(AllAtOncePCBase):
-    """
-    A block Jacobi preconditioner where each block (slice) is built from several timesteps.
-
-    The all-at-once system is split into several slices of several
-    timesteps each (these do not necessarily have to match the 'slices'
-    of the ensemble partition, i.e. the timesteps on a single ensemble
-    member).
-    The preconditioner is constructed as a block-diagonal system where
-    each block is the all-at-once system of a slice. Each block (slice)
-    is (approximately) solved with its own KSP.
-
-    PETSc options:
-
-    'slice_jacobi_nsteps': <int>
-        The number of timesteps per slice. Must be an integer multiple
-        of the number of timesteps on each ensemble member i.e. all
-        timesteps on an ensemble member must belong to the same slice.
-
-    'slice_jacobi_slice_%d': <AllAtOnceSolver options>
-        The solver options for the %d'th LinearSolver for each slice, enumerated globally.
-        Use 'slice_jacobi_slice' to set options for all blocks.
-    """
-    prefix = "slice_jacobi_"
-
+class IntervalJacobiGaussSeidelPCBase(AllAtOncePCBase):
     @profiler()
     def initialize(self, pc, final_initialize=True):
         super().initialize(pc, final_initialize=False)
@@ -45,17 +21,17 @@ class SliceJacobiPC(AllAtOncePCBase):
 
         # all ensemble members must have the same number of timesteps
         if len(set(self.time_partition)) != 1:
-            msg = "SliceJacobiPC only implemented for balanced partitions yet"
+            msg = f"{type(self).__name__} only implemented for balanced partitions yet"
             raise ValueError(msg)
 
         # how many timesteps in each slice?
-        slice_size = PETSc.Options().getInt(
-            f"{self.full_prefix}nsteps")
+        interval_length = PETSc.Options().getInt(
+            f"{self.pc_prefix}interval_length")
 
         # we need to work out how many members of the global ensemble
         # needed to get `split_size` timesteps on each slice ensemble
-        slice_members = slice_size // self.time_partition[0]
-        nslices = self.ntimesteps // slice_size
+        slice_members = interval_length // self.time_partition[0]
+        nslices = self.ntimesteps // interval_length
 
         # create the ensemble for the local slice by splitting the global ensemble
         self.slice_ensemble = split_ensemble(self.ensemble,
@@ -73,40 +49,36 @@ class SliceJacobiPC(AllAtOncePCBase):
         # # # slice aaofuncs - jacobian, yslice # # #
 
         # the slice jacobian is created around a slice
-        # aaofunc that tracks the global aaofunc
+        # aaofunc that views the global aaofunc via val.
         field_function_space = self.aaofunc.field_function_space
-        slice_func = AllAtOnceFunction(
-            self.slice_ensemble, slice_partition, field_function_space,
-            full_function_space=self.aaofunc.function_space,
-            full_dual_space=self.aaofunc.dual_space)
-        slice_func.zero()
-        self.slice_func = slice_func
-
-        for i in range(self.nlocal_timesteps):
-            slice_func[i].assign(self.aaofunc[i])
+        self.slice_func = AllAtOnceFunction(
+            self.slice_ensemble, slice_partition,
+            field_function_space, val=self.aaofunc)
 
         # the result is placed in here and then copied
         # out to the global result
-        self.yslice = slice_func.copy()
+        self.yslice = AllAtOnceFunction(
+            self.slice_ensemble, slice_partition,
+            field_function_space, val=self.y)
 
         # this is the slice rhs
         self.xslice = AllAtOnceCofunction(
             self.slice_ensemble, slice_partition,
-            field_function_space.dual(),
-            full_function_space=self.aaofunc.dual_space,
-            full_dual_space=self.aaofunc.function_space)
+            field_function_space.dual(), val=self.x)
 
         # # # slice aaoform - jacobian, # # #
 
         # here we abuse that aaoform.copy will create
         # the new form over the ensemble of the
         # aaofunc kwarg not its own ensemble.
-        self.slice_form = self.aaoform.copy(aaofunc=slice_func)
+        self.slice_form = self.aaoform.copy(aaofunc=self.slice_func)
 
         # # # slice parameters # # #
-        default_slice_prefix = f"{self.full_prefix}slice_"
+        default_slice_prefix = self.full_prefix
         default_slice_options = get_default_options(
             default_slice_prefix, range(nslices))
+
+        slice_prefix = default_slice_prefix+str(self.slice_rank)
 
         # default to treating the slice as a PC not a KSP.
         # we can't just use dict.setdefault in case the
@@ -121,7 +93,7 @@ class SliceJacobiPC(AllAtOncePCBase):
 
         self.slice_solver = LinearSolver(
             self.slice_form, appctx=self.appctx,
-            options_prefix=default_slice_prefix+str(self.slice_rank),
+            options_prefix=slice_prefix,
             solver_parameters=default_slice_options)
 
         self.initialized = final_initialize
@@ -131,55 +103,89 @@ class SliceJacobiPC(AllAtOncePCBase):
         """
         Update the slice states.
         """
-        # # # update the timestep values
-        aaofunc = self.aaofunc
-        slice_func = self.slice_func
-
-        # slice initial conditions
-        aaofunc.update_time_halos()
-        if self.slice_rank == 0:
-            slice_func.initial_condition.assign(aaofunc.initial_condition)
-        else:
-            slice_func.initial_condition.assign(aaofunc.uprev)
-
-        # slice values
-        for i in range(self.nlocal_timesteps):
-            slice_func[i].assign(aaofunc[i])
-        slice_func.update_time_halos()
+        # Update the timestep values.
+        # slice_func mostly views aaofunc data,
+        # except that slice_func circulant halos
+        # do not necessarily coincide with aaofunc
+        # circulant halos. This means we must ask
+        # slice_func to do the halo update.
+        self.slice_func.update_time_halos()
 
         # # # update the time values
         aaoform = self.aaoform
-        slice_form = self.slice_form
 
         # slice initial time
         if self.slice_rank == 0:
-            slice_form.t0.assign(aaoform.t0)
+            t0 = aaoform.t0
         else:
-            slice_form.t0.assign(aaoform.time[0] - aaoform.dt)
-
-        # slice times
-        for i in range(self.nlocal_timesteps):
-            slice_form.time[i].assign(aaoform.time[i])
+            t0 = float(aaoform.time[0]) - float(aaoform.dt)
+            t0 = self.slice_ensemble.ensemble_comm.bcast(
+                t0, root=0)
+        self.slice_form.time_update(t0)
 
         # # # update the slice
         self.slice_solver.jacobian.update()
 
         return
 
+
+class IntervalJacobiPC(IntervalJacobiGaussSeidelPCBase):
+    """
+    A block Jacobi preconditioner where each block (interval)
+    is built from several timesteps.
+
+    The all-at-once system is split into several intervals of several
+    timesteps each (these do not necessarily have to match the 'slices'
+    of the ensemble partition, i.e. the timesteps on a single ensemble
+    member).
+    The preconditioner is constructed as a block-diagonal system where
+    each block is the all-at-once system of an interval. Each block
+    (interval) is (approximately) solved with its own KSP.
+
+    PETSc options:
+
+    'pc_ijacobi_interval_length': <int>
+        The number of timesteps per interval. Must be an integer multiple
+        of the number of timesteps on each ensemble member i.e. all
+        timesteps on an ensemble member must belong to the same interval.
+
+    'ijacobi_%d': <AllAtOnceSolver options>
+        The solver options for the LinearSolver for the %d-th interval,
+        enumerated globally.
+        Use 'ijacobi_' to set default options for all intervals.
+    """
+    prefix = "ijacobi_"
+
     @profiler()
     def apply_impl(self, pc, x, y):
-
-        # copy global rhs into slice rhs
-        for i in range(self.nlocal_timesteps):
-            self.xslice[i].assign(x[i])
-
         self.yslice.zero()
         self.slice_solver.solve(self.xslice, self.yslice)
 
-        # copy slice result into global result
-        for i in range(self.nlocal_timesteps):
-            y[i].assign(self.yslice[i])
 
+class IntervalGaussSeidelPC(IntervalJacobiGaussSeidelPCBase):
+    """
+    A block Gauss-Seidel preconditioner where each block (interval)
+    is built from several timesteps.
 
-class SliceGaussSeidelPC(AllAtOncePCBase):
+    The all-at-once system is split into several intervals of several
+    timesteps each (these do not necessarily have to match the 'slices'
+    of the ensemble partition, i.e. the timesteps on a single ensemble
+    member).
+    The preconditioner is constructed as a block lower-triangular system
+    where each diagonal block is the all-at-once system of an interval.
+    Each block (interval) is (approximately) solved with its own KSP.
+
+    PETSc options:
+
+    'pc_igs_interval_length': <int>
+        The number of timesteps per interval. Must be an integer multiple
+        of the number of timesteps on each ensemble member i.e. all
+        timesteps on an ensemble member must belong to the same interval.
+
+    'igs_%d': <AllAtOnceSolver options>
+        The solver options for the LinearSolver for the %d-th interval,
+        enumerated globally.
+        Use 'igs_' to set default options for all intervals.
+    """
+    prefix = "igs_"
     pass
