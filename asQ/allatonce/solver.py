c4ea4a4bbbeeae08a469611a394e105c8ae5f452
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2026-02-10 19:25:30 +0000

    OptionsManager from petsctools

--- asQ/allatonce/solver.py
+++ asQ/allatonce/solver.py
@@ -1,4 +1,5 @@
-from firedrake.petsc import PETSc, OptionsManager, flatten_parameters
+from petsctools import OptionsManager, flatten_parameters
+from firedrake.petsc import PETSc
 
 from asQ.profiling import profiler
 from asQ.allatonce import (AllAtOnceCofunction, AllAtOnceFunction,
